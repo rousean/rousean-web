0c0e964fbbbd0d5907ce40ccc82d5d1f2ef7684b
rousean <rousean@163.com>
2022-04-21 22:58:06 +0800

    rousean

---
+++ interview-questions/with-eval的应用.js
@@ -0,0 +1,17 @@
+let greeting = "My name is ${name}, age ${age}, I am a ${job.jobName}"
+let employee = {
+  name: "XiaoMing",
+  age: 11,
+  job: {
+    jobName: "designer",
+    jobLevel: "senior",
+  },
+}
+String.prototype.render = function (obj) {
+  // return this.replace(/\$\{(.*?)\}/g, (item, key) => eval(`obj.${key}`))
+  with (obj) {
+    return eval("`" + this + "`")
+  }
+}
+let result = greeting.render(employee)
+console.log(result)
