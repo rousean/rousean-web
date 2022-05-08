371aca0905cf5ad4cc28bb2794f161dc88e369b5
rousean <rousean@163.com>
2022-05-08 21:49:58 +0800

    web

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <VirtualList></VirtualList>
+</template>
+
+
+<script setup>
+import { defineAsyncComponent } from 'vue'
+const VirtualList = defineAsyncComponent(() => import('./components/VirtualList.vue'))
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ my-vue-app/src/components/VirtualList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="list">
+    <div v-for="item in ip"
+         :key="item">
+      {{item}}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const ip = ref([])
+for (let i = 0; i < 10000; i++) {
+  ip.value.push(Math.random())
+}
+// console.log(ip.value)
+
+</script>
+
+<style>
+.list {
+  width: 300px;
+  height: 600px;
+  border: 1px solid #000;
+  overflow: scroll;
+}
+</style>
