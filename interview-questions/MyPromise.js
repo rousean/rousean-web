4d0988487816961b960925568d524a7bb751eedf
rousean <rousean@163.com>
2022-05-18 22:54:51 +0800

    web

--- interview-questions/MyPromise.js
+++ interview-questions/MyPromise.js
@@ -63,20 +63,24 @@ export class MyPromise {
       }
       if (this.states === "pending") {
         this.onFulfilledCallback.push(() => {
-          try {
-            let x = onFulfilled(this.value)
-            resolvePromise(promise2, x, resolve, reject)
-          } catch (e) {
-            reject(e)
-          }
+          setTimeout(() => {
+            try {
+              let x = onFulfilled(this.value)
+              resolvePromise(promise2, x, resolve, reject)
+            } catch (e) {
+              reject(e)
+            }
+          }, 0)
         })
         this.onRejectedCallback.push(() => {
-          try {
-            let x = onRejected(this.reason)
-            resolvePromise(promise2, x, resolve, reject)
-          } catch (e) {
-            reject(e)
-          }
+          setTimeout(() => {
+            try {
+              let x = onRejected(this.reason)
+              resolvePromise(promise2, x, resolve, reject)
+            } catch (e) {
+              reject(e)
+            }
+          }, 0)
         })
       }
     })
@@ -86,12 +90,101 @@ export class MyPromise {
   catch(errorCallback) {
     return this.then(null, errorCallback)
   }
+
+  finally(callback) {
+    return this.then(
+      value => {
+        return MyPromise.resolve(callback()).then(() => {
+          return value
+        })
+      },
+      reason => {
+        return MyPromise.reject(callback()).then(() => {
+          throw reason
+        })
+      }
+    )
+  }
+
+  static resolve(value) {
+    if (value instanceof MyPromise) {
+      return value
+    }
+    return new MyPromise((resolve, reject) => {
+      if (value && value.then && typeof value.then === "function") {
+        setTimeout(() => {
+          value.then(resolve, reject)
+        }, 0)
+      } else {
+        resolve(value)
+      }
+    })
+  }
+
+  static reject(reason) {
+    return new MyPromise((resolve, reject) => {
+      reject(reason)
+    })
+  }
+
+  // Promise.all(promises)返回一个promise对象
+  // 如果传入的参数是一个空的可迭代对象,那么此promise对象回调完成(resolve),只有此情况,是同步执行的,其它都是异步返回的
+  // 如果传入的参数不包含任何promise,则返回一个异步完成
+  // promises中所有的promise都完成时或参数中不包含promise时,那么Promise.all返回的promise对象完成
+  // 如果参数中有一个promise失败,那么Promise.all返回的promise对象失败
+  // 在任何情况下,Promise.all返回的promise的完成状态的结果都是一个数组
+  static all(promises) {
+    return new MyPromise((resolve, reject) => {
+      let index = 0
+      let result = []
+      if (promises.length === 0) {
+        resolve(result)
+      } else {
+        debugger
+        for (let i = 0; i < promises.length; i++) {
+          MyPromise.resolve(promises[i]).then(
+            value => {
+              result[i] = value
+              if (++index === promises.length) {
+                resolve(result)
+              }
+            },
+            reason => {
+              reject(reason)
+              return
+            }
+          )
+        }
+      }
+    })
+  }
+
+  static race(promises) {
+    return new MyPromise((resolve, reject) => {
+      if (promises.length === 0) {
+        return
+      } else {
+        for (let i = 0; i < promises.length; i++) {
+          MyPromise.resolve(promises[i]).then(
+            value => {
+              resolve(value)
+              return
+            },
+            reason => {
+              reject(reason)
+              return
+            }
+          )
+        }
+      }
+    })
+  }
 }
 
 function resolvePromise(promise2, x, resolve, reject) {
   let called = false
   if (promise2 === x) {
-    reject(new Error("Chaining cycle detected for promise #<Promise>"))
+    reject(new Error("Chaining cycle detected for promise #<MyPromise>"))
   }
   if ((typeof x === "object" && x !== null) || typeof x === "function") {
     // then是函数的情况
@@ -101,16 +194,12 @@ function resolvePromise(promise2, x, resolve, reject) {
         then.call(
           x,
           y => {
-            if (called) {
-              return
-            }
+            if (called) return
             called = true
             resolvePromise(promise2, y, resolve, reject)
           },
           r => {
-            if (called) {
-              return
-            }
+            if (called) return
             called = true
             resolvePromise(promise2, r, resolve, reject)
           }
@@ -119,9 +208,7 @@ function resolvePromise(promise2, x, resolve, reject) {
         resolve(x)
       }
     } catch (e) {
-      if (called) {
-        return
-      }
+      if (called) return
       called = true
       reject(e)
     }
